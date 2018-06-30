6ca4c580fee27013685b18f1da7bc4946f8c2821
JaapN <nieuwenhuizenjaap@gmail.com>
2018-06-30 16:46:23 +0200

    Update lambdacalculus_template.py

--- lambdacalculus_template.py
+++ lambdacalculus_template.py
@@ -7,16 +7,16 @@ class LambdaTerm:
 
     # define usable variable names
     varchars = ['u', 'x', 'y', 'z']
-
+    
     def find_parentheses(s):
         """ Find and return the location of the matching parentheses pairs in s.
-
         Given a string, s, return a dictionary of start: end pairs giving the
-        indexes of the matching parentheses in s. Suitable exceptions are
+        indices of the matching parentheses in s. Suitable exceptions are
         raised if s contains unbalanced parentheses.
+        Source: https://scipython.com/blog/parenthesis-matching-in-python/
         """
 
-        # The indexes of the open parentheses are stored in a stack, implemented
+        # The indices of the open parentheses are stored in a stack, implemented
         # as a list
 
         stack = []
@@ -35,27 +35,45 @@ class LambdaTerm:
                              'at index {}'.format(stack.pop()))
         return parentheses_locs
     
+   
     # returns a lambda term created from the string argument
     # categorize lambda terms using brackets, working from outside in
     @staticmethod
     def fromstring(string):
+        # define characters for later convenience
+        lambdastring = ''
+        l = '('
+        r = ')'
+        
+        # collect index pairs for every bracket
         for s in enumerate(string, start=1):
             # find parentheses indices
             try:
                 parentheses_locs = find_parentheses(s)
-                sorted([(k,v) for k, v in parentheses_locs.items()])
+                indexpairs = sorted([(k,v) for k, v in parentheses_locs.items()])
             # parentheses do not match
             except IndexError as e:
                 print(str(e))
         
-        https://scipython.com/blog/parenthesis-matching-in-python/
+        # define indices of the second-to-last right parenthesis and corresponding left parenthesis
+        rpair = indexpairs[-2][1]
+        lpair = indexpairs[-2][0]
         
+        # application if space after second-to-last right bracket
+        if string[rpair+1] == ' ':
+            lambdastring = Application.frstring(string)
+        # abstraction
+        elif string[lpair+1] == l and string[lpair+2] == lam:
+            lambdastring = Abstraction.frstring()
+        # variable if all characters are correct
+        else:
+            for i in range(len(string)):
+                if string[i] not in varchars:
+                    raise NameError
+        
+        return lambdastring
+   
         '''
-        # define characters for later convenience
-        lambdat = ''
-        l = '('
-        r = ')'
-        
         # find most right bracket after removing outer brackets
         string.strip(r)
         nextr = string.rfind(r)
@@ -71,8 +89,8 @@ class LambdaTerm:
             # abstraction if (λ follows bracket
             if string[nextl+1] == l and string[nextl+2] == lam:
                 lambdat = Abstraction.frstring()
-
-            else: # Correct variable if all the characters in it are correct characters
+        # variabele als alle karakters correct zijn
+        else: # Correct variable if all the characters in it are correct characters
                 for i in range(len(string)):
                     if string[i] not in varchars: # if there is an incorrect character, stop and raise exception
                         raise NameError
